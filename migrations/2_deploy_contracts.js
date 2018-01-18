338dde714bd4da98318d4748dcef7a0e9222d95e
Isan-Rivkin <isanrivkin@gmail.com>
2018-01-18 12:55:26 -0800

    refactoring enigma token and adding open-zeppelin contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,13 @@
-//var DummyToken = artifacts.require("./token/DummyToken.sol");
+var DummyToken = artifacts.require("./token/DummyToken.sol");
 var MarketPlace = artifacts.require("./MarketPlace.sol");
 
 module.exports = function(deployer) {
-  // deployer.then(()=>{
-  // 	var addr2= '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
-  // 	return deployer.deploy(DummyToken,addr2);
-  // }).then(()=>{
-  // 	return DummyToken.deployed();
-  // }).then(tokenInstance=>{
-  // 	return deployer.deploy(MarketPlace, tokenInstance.address, 123);
-  // });
+  deployer.then(()=>{
+  	var addr2= '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
+  	return deployer.deploy(DummyToken,addr2);
+  }).then(()=>{
+  	return DummyToken.deployed();
+  }).then(tokenInstance=>{
+  	return deployer.deploy(MarketPlace, tokenInstance.address, 123);
+  });
 };
