6707b746de67fead57fee44b392f7bb67ffad4b7
Isan-Rivkin <isanrivkin@gmail.com>
2018-01-21 04:58:31 -0800

    ropsten config truffle.js

--- truffle.js
+++ truffle.js
@@ -1,9 +1,30 @@
+
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var DummyMnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+
 module.exports = {
     networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(DummyMnemonic, "https://ropsten.infura.io/0oo5uLogrmd5FxaRoiyK")
+      },
+      network_id: 3,
+      gas: 99999999999999,
+    } 
   },
+ 
+  // solc: {
+  // optimizer: {
+  //   enabled: true,
+  //   runs: 200
+  // 	}
+  // }
 };
+
+
