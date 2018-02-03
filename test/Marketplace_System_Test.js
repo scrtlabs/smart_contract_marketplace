fe8935603db46ff46e2ffa7a602647a3c4d53b9d
Isan-Rivkin <isanrivkin@gmail.com>
2018-02-03 09:30:53 -0800

    TST:round trip tests added

--- test/Marketplace_System_Test.js
+++ test/Marketplace_System_Test.js
@@ -9,8 +9,6 @@ const simple = true;
 const mock = true;
 const system_test = true;
 
-
-
 contract('Marketplace Mock', function(accounts) {
 const emptyAddress ="0x0000000000000000000000000000000000000000000000000000000000000000";
  // provider 1
@@ -97,7 +95,7 @@ const emptyAddress ="0x000000000000000000000000000000000000000000000000000000000
       });
     });
 
- if(simple && system_test && true)
+ if(simple && mock && system_test && true)
   it("Register (mock) expired data set, refund and withdraw",()=>{
     return EnigmaToken.deployed().then(instance=>{
       relativePunish =2;
@@ -146,49 +144,37 @@ const emptyAddress ="0x000000000000000000000000000000000000000000000000000000000
   });
 
   if(simple && system_test && true)
-    it("Should register 2 subscribers to 3 data setes",()=>{
-      return EnigmaToken.deployed().then(instance=>{
-        enigma = instance;
-        return Marketplace.deployed().then(instance=>{
-          mp = instance;
-          return enigma.approve(mp.address,price1,{from:subscriber1});
-        }).then(tx=>{
-          return enigma.increaseApproval(mp.address,price2,{from:subscriber1});
-        }).then(tx=>{
-          return enigma.approve(mp.address,price3,{from:subscriber2});
-        }).then(tx=>{
-          return enigma.allowance(subscriber1,mp.address);
-        }).then(allowed=>{
-          assert.equal(allowed.toNumber(),price1+price2,"allowed is not equal to price");
-          return enigma.allowance(subscriber2,mp.address);
-        }).then(allowed=>{
-          assert.equal(allowed.toNumber(),price3,"allowed is not equal to price");
-          return mp.subscribe(data1,{from:subscriber1});
-        }).then(tx=>{
-          return mp.subscribe(data2,{from:subscriber1});
-        }).then(tx=>{
-          return mp.subscribe(data3,{from:subscriber2});
-        }).then(tx=>{
-          return mp.checkAddressSubscription.call(subscriber1,data1);
-        }).then(subscription=>{
-          var s1 = parseSubscription(subscription);
-          var equal = isEqualSubscriptions(s1,{susbcriber:subscriber1,dataName:data1,price:price1,isUnExpired:true,isPaid:false,isPunishedProvider:false,isOrder:true});
-          assert.equal(equal,true,"Subscription1 is not equal");
-          return mp.checkAddressSubscription.call(subscriber1,data2);
-        }).then(subscription=>{
-          var s2 = parseSubscription(subscription);
-          var equal = isEqualSubscriptions(s2,{susbcriber:subscriber1,dataName:data2,price:price2,isUnExpired:true,isPaid:false,isPunishedProvider:false,isOrder:true});
-          assert.equal(equal,true,"Subscription2 is not equal");
-          return mp.checkAddressSubscription.call(subscriber2,data3);
-        }).then(subscription=>{
-          var s3 = parseSubscription(subscription);
-          var equal = isEqualSubscriptions(s3,{susbcriber:subscriber2,dataName:data3,price:price3,isUnExpired:true,isPaid:false,isPunishedProvider:false,isOrder:true});
-          assert.equal(equal,true,"Subscription3 is not equal");
-        });
-      });
+    it("Should register 2 subscribers to 3 data setes",async function(){
+      let enigma = await EnigmaToken.deployed();
+      let marketPlace = await Marketplace.deployed();
+      // approve the Marketplace contract as a spender
+      await enigma.approve(mp.address,price1,{from:subscriber1});
+      await enigma.increaseApproval(mp.address,price2,{from:subscriber1});
+      await enigma.approve(mp.address,price3,{from:subscriber2});
+      let allowance1 = await enigma.allowance(subscriber1,mp.address);
+      let allowance2 = await enigma.allowance(subscriber2,mp.address);
+      assert.equal(allowance1.toNumber(),price1+price2,"allowed is not equal to price");
+      assert.equal(allowance2.toNumber(),price3,"allowed is not equal to price");
+      // subscribe to data 
+      await marketPlace.subscribe(data1,{from:subscriber1});
+      await marketPlace.subscribe(data2,{from:subscriber1});
+      await marketPlace.subscribe(data3,{from:subscriber2});
+      // validate subscription details 
+      let subscription1 = await marketPlace.checkAddressSubscription.call(subscriber1,data1);
+      let subscription2 = await marketPlace.checkAddressSubscription.call(subscriber1,data2);
+      let subscription3 = await marketPlace.checkAddressSubscription.call(subscriber2,data3);
+      let s1 = parseSubscription(subscription1);
+      let equal = isEqualSubscriptions(s1,{subscriber:subscriber1,dataName:data1,price:price1,isUnExpired:true,isPaid:false,isPunishedProvider:false,isOrder:true});
+      assert.equal(equal,true,"Subscription1 is not equal");
+      let s2 = parseSubscription(subscription2);
+      equal = isEqualSubscriptions(s2,{subscriber:subscriber1,dataName:data2,price:price2,isUnExpired:true,isPaid:false,isPunishedProvider:false,isOrder:true});
+      assert.equal(equal,true,"Subscription2 is not equal");
+      let s3 = parseSubscription(subscription3);
+      equal = isEqualSubscriptions(s3,{subscriber:subscriber2,dataName:data3,price:price3,isUnExpired:true,isPaid:false,isPunishedProvider:false,isOrder:true});
+      assert.equal(equal,true,"Subscription3 is not equal");
     });
 
-  if(simple && system_test && true)
+  if(simple && mock && system_test && true)
     it("Should try subscribing to an expired and punished data set and fail",async function(){
       let marketPlace = await Marketplace.deployed();
       try{
@@ -198,7 +184,7 @@ const emptyAddress ="0x000000000000000000000000000000000000000000000000000000000
         return true;
       }
     });
-  if(simple && system_test && true)
+  if(simple && mock && system_test && true)
     it("Should test for an expired subscription",async function(){
       let marketPlace = await Marketplace.deployed();
       let subsInfo = await marketPlace.checkAddressSubscription.call(subscriberRefund,dataExpiredAndPunished);
@@ -241,12 +227,51 @@ const emptyAddress ="0x000000000000000000000000000000000000000000000000000000000
       current = await marketPlace.mOwner.call();
       assert.equal(current,accounts[0],"Ownership did not change back");
     });
-
+  if(simple && system_test && true)
+    it("Should punish provider -> subscribe and fail -> unpunish -> subscribe",async function(){
+      let enigma = await EnigmaToken.deployed();
+      let marketPlace = await Marketplace.deployed();
+      // approve the Marketplace contract as a spender
+      await enigma.increaseApproval(mp.address,price3,{from:subscriber1});
+      let punish = true;
+      await marketPlace.setPunishProvider(data3,punish,{from:accounts[0]});
+      try{
+        await marketPlace.subscribe(data3,{from:subscriber1});
+        assert.equal(false,true,"Subscribed to punished provider");
+      }catch(e){
+         await marketPlace.setPunishProvider(data3,!punish,{from:accounts[0]});
+         await marketPlace.subscribe(data3,{from:subscriber1});
+         let info =await marketPlace.checkAddressSubscription.call(subscriber1,data3);
+         info = parseSubscription(info);
+         let res = isEqualSubscriptions(info,{subscriber:subscriber1,dataName:data3,price:price3,isUnExpired:true,isPaid:false,isPunishedProvider:false,isOrder:true});
+         assert.equal(true,res,"Subscription is not equal");
+      }
+    });
+  if(simple && system_test && true)
+    it("Should update activiy", async function(){
+      let isActive = false;
+      let marketPlace = await Marketplace.deployed();
+      await marketPlace.changeDataSourceActivityStatus(data3,isActive,{from:owner3});
+      let info = await marketPlace.getDataProviderInfo.call(data3);
+      assert.equal(isActive,parseProvider(info).isActive,"Activity did not change");
+      await marketPlace.changeDataSourceActivityStatus(data3,!isActive,{from:owner3});
+      info = await marketPlace.getDataProviderInfo.call(data3);
+      assert.equal(!isActive,parseProvider(info).isActive,"Activity did not change");
+    });
+  if(simple && system_test && true)
+    it("should change providers price",async function(){
+      let newPrice = 120;
+      let marketPlace = await Marketplace.deployed();
+      await marketPlace.updateDataSourcePrice(data1,newPrice,{from:owner1});
+      let info = await marketPlace.getDataProviderInfo(data1);
+      assert.equal(parseProvider(info).price,newPrice,"price did not change");
+    });
+  //
 });
 
 function parseSubscription(info){
   var normal = {};
-  normal.susbcriber = info[0];
+  normal.subscriber = info[0];
   normal.dataName = utils.toAscii(info[1]);
   normal.price = info[2].toNumber();
   normal.startTime = info[3].toNumber();
@@ -268,7 +293,7 @@ function isEqualSubscriptions(sub1,sub2){
 }
 function printSubscriptionInfo(info){
   var s = parseSubscription(info);
-  console.log("susbcriber : " +s.susbcriber );
+  console.log("subscriber : " +s.subscriber );
   console.log("dataName : " +s.dataName );
   console.log("price : " + s.price);
   console.log("startTime : " + s.startTime);
@@ -278,21 +303,35 @@ function printSubscriptionInfo(info){
   console.log("isPunishedProvider : " + s.isPunishedProvider);
   console.log("isOrder : " + s.isOrder);
 }
+function isEqualProvider(p1,p2){
+  return p1.owner == p1.owner &&
+  p1.price == p1.price &&
+  p1.volume == p1.volume &&
+  p1.subscriptionsNum ==p1.subscriptionsNum &&
+  p1.isProvider ==p1.isProvider &&
+  p1.isActive ==p1.isActive &&
+  p1.isPunished ==p1.isPunished;
+}
+function parseProvider(info){
+  var pro = {};
+  pro.owner = info[0];
+  pro.price = info[1].toNumber();
+  pro.volume = info[2].toNumber();
+  pro.subscriptionsNum = info[3].toNumber();
+  pro.isProvider =info[4];
+  pro.isActive = info[5];
+  pro.isPunished = info[6];
+  return pro;
+}
 function printProviderInfo(info){
-        var owner = info[0];
-        var price = info[1].toNumber();
-        var volume = info[2].toNumber();
-        var subscriptionsNum = info[3].toNumber();
-        var isProvider =info[4];
-        var isActive = info[5];
-        var isPunished = info[6];
-        console.log("owner: " + owner);
-        console.log("price : " + price);
-        console.log("volume : " + volume);
-        console.log("subscsnum : " + subscriptionsNum);
-        console.log("isProvider: " +isProvider );
-        console.log("isActive : " + isActive);
-        console.log("isPunished : " +isPunished );
+        var p = parseProvider(info);
+        console.log("owner: " + p.owner);
+        console.log("price : " + p.price);
+        console.log("volume : " + p.volume);
+        console.log("subscsnum : " + p.subscriptionsNum);
+        console.log("isProvider: " +p.isProvider );
+        console.log("isActive : " + p.isActive);
+        console.log("isPunished : " +p.isPunished );
 }
 
 
--- truffle.js
+++ truffle.js
@@ -7,7 +7,7 @@ module.exports = {
     networks: {
     development: {
       host: "127.0.0.1",
-      port: 8545,
+      port: 9545,//8545,
       network_id: "*" // Match any network id
     },
     ropsten: {
@@ -15,11 +15,11 @@ module.exports = {
         return new HDWalletProvider(DummyMnemonic, "https://ropsten.infura.io/0oo5uLogrmd5FxaRoiyK")
       },
       network_id: 3,
-      gas: 99999999999999,
+      gas: 999999999999999,
     } 
   },
   // solc: {
-  // optimizer: {
+  // optimizer: { 
   //   enabled: true,
   //   runs: 200
   // 	}
